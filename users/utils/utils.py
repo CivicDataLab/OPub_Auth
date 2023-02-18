3fe98c7d3b7e9b85a202edecd49fe8769bd3c9a1
Abhinav <a.kumar2102@gmail.com>
2023-02-18 17:06:38 +0530

    add util for checking requestor user role

--- users/utils/utils.py
+++ users/utils/utils.py
@@ -19,7 +19,7 @@ def check_user_role(calling_user, org_id):
     else:
         org_roleobj = UserRole.objects.filter(org_id=org_id)
         org_role_list = [each.role.role_name for each in org_roleobj]
-        if "DPA" not in org_role_list:
+        if "DPA" not in org_role_list and len(org_role_list) > 0:
             isdpa = is_usr_parent_org_dpa(calling_user, org_id)
 
     if "DP" in role_list:
